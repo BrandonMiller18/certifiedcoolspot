9010c9e9dc7ba864d24468b70b448e62b90352dd
Brandon Miller <blmiller0809@gmail.com>
2023-08-11 17:57:19 -0500

    adjust styles, mobile

--- themes/certifiedcoolspot/archive.php
+++ themes/certifiedcoolspot/archive.php
@@ -5,7 +5,7 @@
 
 <div class="page-wrap">
 
-    <div class="section dark divider-top-btm-accent archive-hero">
+    <div class="section dark divider-top-btm-accent">
         <div>
             <h1 class="section-heading">Cool Spot Archive.</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quam!</p>
@@ -15,7 +15,7 @@
     
     <?php  get_template_part('includes/section', 'featured_spot') ?>
 
-    <div class="section dark divider-top-btm-accent-two">
+    <div class="section divider-bottom-accent-two">
         <?php  get_template_part('includes/section', 'locations') ?>
     </div>
 
